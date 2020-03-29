1b1d86a230d4c952c1bce665f1f730d6cd8c45af
Lincoln César Medina de Oliveira <lincoln.oliveira@cmc.pr.gov.br>
2020-03-29 11:02:12 -0300

    criação de usuário

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,31 @@
+import User from '../models/User';
+
+class UserController {
+    // semelhante a um middleware
+    async store(req, res) {
+        // verifica se o email do usuário já está cadastrado
+        const userExists = await User.findOne({
+            where: { email: req.body.email },
+        });
+        if (userExists) {
+            return res
+                .status(400)
+                .json({ error: 'Usuário com este e-mail já existe.' });
+        }
+        // pegaria/retornaria todos os campos do body
+        // const user = await User.create(req.body);
+        // return res.json(user);
+
+        // mas quer retornar apenas alguns campos
+        const { id, name, email, provider } = await User.create(req.body);
+
+        return res.json({
+            id,
+            name,
+            email,
+            provider,
+        });
+    }
+}
+
+export default new UserController();
--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,12 @@
 // importando apenas a parte de roteamento do express
 // const { Router } = require('express');
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-// routes.get('/', (req, res) => res.json({ message: 'Olá povinho!' }));
-
-// Teste do loader do model, criando um usuário
-// a função deve ser async para aguardar o banco fazer o insert, poder usar o await
-routes.get('/', async (req, res) => {
-    const user = await User.create({
-        name: 'Diego Fernandes',
-        email: 'diego@rocketseat.com.br',
-        password_hash: '1238712387',
-    });
-    return res.json(user);
-});
+// rota post, para incluir, passa a função store do controller, como um middleware
+routes.post('/users', UserController.store);
 
 // module.exports = routes;
 export default routes;
