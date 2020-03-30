f1bfe0bdd4afadf84962b971ae2a16993fe303b3
Lincoln César Medina de Oliveira <lincoln.oliveira@cmc.pr.gov.br>
2020-03-30 11:47:07 -0300

    hash da senha

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
 
 class User extends Model {
     static init(sequelize) {
@@ -6,6 +7,7 @@ class User extends Model {
             {
                 name: Sequelize.STRING,
                 email: Sequelize.STRING,
+                password: Sequelize.VIRTUAL, // campo que ñ será armazenado no BD
                 password_hash: Sequelize.STRING,
                 provider: Sequelize.BOOLEAN,
             },
@@ -13,6 +15,16 @@ class User extends Model {
                 sequelize,
             }
         );
+
+        // adciona um hook, como um trigger, a ser executado qdo ação for executada
+        this.addHook('beforeSave', async (user) => {
+            if (user.password) {
+                // gera o hash a partir da passwor passada. 8 é o nº de iterações de hash(?)
+                user.password_hash = await bcrypt.hash(user.password, 8);
+            }
+        });
+
+        return this;
     }
 }
 
