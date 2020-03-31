566b3806ac84ea4e0b08a2d8788a8e10e7bb4985
Lincoln César Medina de Oliveira <lincoln.oliveira@cmc.pr.gov.br>
2020-03-31 00:19:56 -0300

    validando campos de entrada

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,10 +1,20 @@
 import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
 
 import User from '../models/User';
 import authConfig from '../../config/auth';
 
 class SessionController {
     async store(req, res) {
+        const schema = Yup.object().shape({
+            email: Yup.string().required().email(),
+            password: Yup.string().required(),
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Campos inválidos.' });
+        }
+
         const { email, password } = req.body;
         // procura usuário
         const user = await User.findOne({ where: { email } });
--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,8 +1,21 @@
+// biblioteca de validação
+// não tem um export default, então importa tudo para este Yup
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
     // semelhante a um middleware
     async store(req, res) {
+        // defino o shape do objeto que vou querer
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            email: Yup.string().required().email(),
+            password: Yup.string().required().min(6),
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Campos inválidos.' });
+        }
         // verifica se o email do usuário já está cadastrado
         const userExists = await User.findOne({
             where: { email: req.body.email },
@@ -28,13 +41,32 @@ class UserController {
     }
 
     async update(req, res) {
+        // defino o shape do objeto que vou querer
+        const schema = Yup.object().shape({
+            name: Yup.string(),
+            email: Yup.string().email(),
+            password: Yup.string().min(6),
+            oldPassword: Yup.string()
+                // teste dependente do password. Se password existe, field.required
+                .when('password', (password, field) =>
+                    password ? field.required() : field
+                ),
+            confirmPassword: Yup.string().when('password', (password, field) =>
+                // semelhante, se há password, tem que ter confirmPassword com o mesmo valor de password
+                password ? field.required().oneOf([Yup.ref('password')]) : field
+            ),
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Campos inválidos.' });
+        }
+
         const { email, oldPassword, password } = req.body;
         const user = await User.findByPk(req.userId);
 
         // só vai verificar duplicidade de email se estiver mudando
         if (email && email !== user.email) {
             const exists = await User.findOne({ where: { email } });
-
             if (exists) {
                 return res
                     .status(400)
