4da8f8a665392c12849744a9fe3a9ecc815415f9
Lincoln César Medina de Oliveira <lincoln.oliveira@cmc.pr.gov.br>
2020-04-25 12:29:45 -0300

    tratamento de erros, sentry e youch

--- src/app.js
+++ src/app.js
@@ -1,25 +1,27 @@
-// A versão atual do Node, ainda não tem o import/export.
-// A sintaxe então seria assim:
-// const express = require ('express');
-// const routes = require ('./routes');
-
-// Mas dá para usar algumas bibliotecas que traduzem o código para versões mais
-// antigas do javascript, como o babel e o sucrase. Vamos usar o sucrase
 import express from 'express';
 import path from 'path';
-import routes from './routes';
+import * as Sentry from '@sentry/node';
+import 'express-async-errors'; // para o express capturar erros de async e poder passar ao sentry
+import Youch from 'youch';
 
+import sentryConfig from './config/sentry';
+import routes from './routes';
 import './database';
 
 class App {
     constructor() {
         this.server = express();
 
+        // sw de monitoramento da aplicação
+        Sentry.init(sentryConfig);
+
         this.middlewares();
         this.routes();
+        this.exceptionHandler();
     }
 
     middlewares() {
+        this.server.use(Sentry.Handlers.requestHandler()); // antes da rotas
         this.server.use(express.json());
         // este middle cria um "rota" de acesso para conteúdo estático,
         // no caso, a rota /files trará os arquivos do diretótio ../tmp/uploads
@@ -31,10 +33,18 @@ class App {
 
     routes() {
         this.server.use(routes);
+        this.server.use(Sentry.Handlers.errorHandler()); // depois da rotas
+    }
+
+    // tto de erro usando o youch, vai retornar um erro em formato json para o cliente
+    exceptionHandler() {
+        // qdo o middleware recebe 4 parâmetros, o express sabe que é de tto de exceção
+        this.server.use(async (err, req, res, next) => {
+            const errors = await new Youch(err, req).toJSON();
+
+            return res.status(500).json(errors); // erro 500, erro interno do servidor
+        });
     }
 }
 
-// sintaxe antiga:
-// module.exports = new App.server/
-// com o sucrase:
 export default new App().server;
---
+++ src/config/sentry.js
@@ -0,0 +1,4 @@
+export default {
+    dsn:
+        'https://30bc729050f9412583f560a260800b8b@o383363.ingest.sentry.io/5213436',
+};
